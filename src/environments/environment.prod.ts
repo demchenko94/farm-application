dbe03a6b9a5da26b69f477d09854a8a4b1569b1f
Demchenko Yevhenii <yevhenii.demchenko@paymash.com>
2022-08-19 12:30:51 +0300

    change prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5',
+  openWeatherApiKey: '8ab15df98c76daa16b88e287c611d432',
+  googlePlaceApiUrl: 'https://maps.googleapis.com/maps/api',
+  googlePlaceApiKey: 'AIzaSyC41BQy3_Pb2A7Uep8NKE5HDby74kDPCog'
 };
